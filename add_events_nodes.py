60e46102107387faa2ae24b924cc77f37170065c
nishantparekh01 <162311263+nishantparekh01@users.noreply.github.com>
2024-08-01 23:15:54 +0200

    Add node to node_list in xml in AST

--- add_events_nodes.py
+++ add_events_nodes.py
@@ -21,20 +21,48 @@ def add_events_to_xml(event): # event is a string
 
 
 # A function to add nodes to node_list
-def add_nodes_to_xml(node):
+def add_nodes_to_xml(node_list):
+    # Exameple of node_list = ['S0', 'S1', 'S2', 'S3']
+    NodeList = ET.Element("NodeList")
+    for node in node_list:
+        if node == 'S0':
+            SimpleNode = ET.SubElement(NodeList, "SimpleNode", Initial = "true",  Name = node)
+            EventList = ET.SubElement(SimpleNode, "EventList")
+            SimpleIdentifier_accepting = ET.SubElement(EventList, "SimpleIdentifier", Name = ":accepting")
+        else:
+            SimpleNode = ET.SubElement(NodeList, "SimpleNode",  Name = node)
+
+    return  NodeList
+
+
+# Creating a list to add all the nodes for a specific efsm
+#efsm_node_list = []
+
+def add_node_to_efsm_node_list(source_node, target_node):
+    if source_node not in efsm_node_list:
+        efsm_node_list.append(source_node)
+
+    if target_node not in efsm_node_list:
+        efsm_node_list.append(target_node)
 
 
 
 for efsm in pre_supremica['Components']:
     #print(efsm)
+    #global efsm_node_list
+    efsm_node_list = []
 
     if 'edge_list' in pre_supremica['Components'][efsm]:
 
         n_transitions = len(pre_supremica['Components'][efsm]['edge_list'])
 
         if n_transitions == 1:
-            pre_supremica['Components'][efsm]['edge_list']['t0']['source_index'] = 'S0'
-            pre_supremica['Components'][efsm]['edge_list']['t0']['target_index'] = 'S0'
+            source_node = 'S0'
+            target_node = source_node
+            pre_supremica['Components'][efsm]['edge_list']['t0']['source_index'] = source_node
+            pre_supremica['Components'][efsm]['edge_list']['t0']['target_index'] = target_node
+            add_node_to_efsm_node_list(source_node, target_node)
+
             # adding events only if the events list is empty
             if not pre_supremica['Components'][efsm]['edge_list']['t0']['events']:
                 event_name = str(efsm + '1')
@@ -43,8 +71,12 @@ for efsm in pre_supremica['Components']:
         else:
             for i in range(n_transitions):
                 if i == 0:
-                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['source_index'] = 'S0'
-                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['target_index'] = 'S1'
+                    source_node = 'S0'
+                    target_node = 'S1'
+                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['source_index'] = source_node
+                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['target_index'] = target_node
+                    add_node_to_efsm_node_list(source_node, target_node)
+
                     # adding events only if the events list is empty
                     if not pre_supremica['Components'][efsm]['edge_list'][f't{i}']['events']:
                         event_name = str(efsm + '1')
@@ -52,21 +84,33 @@ for efsm in pre_supremica['Components']:
                         add_events_to_xml(event_name)
 
                 elif i == n_transitions - 1:
-                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['source_index'] = 'S' + str(i)
-                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['target_index'] = 'S0'
+                    source_node = 'S' + str(i)
+                    target_node = 'S0'
+                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['source_index'] = source_node
+                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['target_index'] = target_node
+                    add_node_to_efsm_node_list(source_node, target_node)
+
                     # adding events only if the events list is empty
                     if not pre_supremica['Components'][efsm]['edge_list'][f't{i}']['events']:
                         event_name = str(efsm + 'X')
                         pre_supremica['Components'][efsm]['edge_list'][f't{i}']['event'] = event_name
                         add_events_to_xml(event_name)
                 else:
-                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['source_index'] = 'S' + str(i)
-                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['target_index'] = 'S' + str(i+1)
+                    source_node = 'S' + str(i)
+                    target_node = 'S' + str(i+1)
+                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['source_index'] = source_node
+                    pre_supremica['Components'][efsm]['edge_list'][f't{i}']['target_index'] = target_node
+                    add_node_to_efsm_node_list(source_node, target_node)
+
                     # adding events only if the events list is empty
                     if not pre_supremica['Components'][efsm]['edge_list'][f't{i}']['events']:
                         event_name = str(efsm + str(i+1))
                         pre_supremica['Components'][efsm]['edge_list'][f't{i}']['event'] = event_name
                         add_events_to_xml(event_name)
+
+    # Adding the node_list to pre_supremica
+    nodes_xml = add_nodes_to_xml(efsm_node_list)
+    pre_supremica['Components'][efsm]['node_list'] = ET.tostring(nodes_xml, encoding='unicode', method='xml')
 
 
 # Adding all the events to pre_supremica
